8277414cf63d16b6d9730cae2c81b280fc20d881
huangpeng <huangpeng@meizu.com>
2020-04-05 17:28:11 +0800

    搜索表单请求接口功能完成 #0

--- src/page/factoryScreen.vue
+++ src/page/factoryScreen.vue
@@ -60,6 +60,174 @@
           </el-form-item>
         </el-form>
       </el-tab-pane>
+
+      <el-tab-pane
+        label="搜索表单"
+        name="searchForm">
+        <h3>基本配置</h3>
+        <el-form
+          :model="searchForm"
+          :rules="searchFormRules"
+          label-width="160px">
+          <el-form-item
+            label="搜索表单标题"
+            prop="title">
+            <el-input v-model="searchForm.title"/>
+          </el-form-item>
+          <el-form-item label="表单标签宽度">
+            <el-input v-model="searchForm.labelWidth"/>
+          </el-form-item>
+          <el-form-item
+            label="页面是否立即加载列表"
+            prop="url">
+            <el-switch
+              v-model="searchForm.immediate"
+              active-text="是"
+              inactive-text="否"/>
+          </el-form-item>
+        </el-form>
+        <hr>
+        <h3>请求接口</h3>
+        <el-table :data="searchFormUrls">
+          <el-table-column
+            label="类型"
+            align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.type | searchFormUrlTypeFilter }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="是否已配置"
+            align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.config ? '是' : '否' }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="路径"
+            prop="url"/>
+          <el-table-column
+            label="方式"
+            align="center"
+            prop="method"/>
+          <el-table-column
+            label="参数值"
+            align="center"
+            prop="params"/>
+          <el-table-column
+            label="操作"
+            align="center">
+            <template slot-scope="scope">
+              <el-button
+                type="text"
+                @click="handleClearUrlConfig('searchForm', scope.row)">
+                清空
+              </el-button>
+              <el-button
+                type="text"
+                @click="handleSearchFormUrlConfig(scope.row)">
+                {{ scope.row.config ? '编辑' : '配置' }}
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <hr>
+        <h3>
+          表单配置
+          <el-button
+            size="small"
+            type="success"
+            icon="el-icon-plus"
+            @click="handleSearchFormFieldAdd"/>
+        </h3>
+        <el-table :data="searchForm.fields">
+          <el-table-column
+            label="标题"
+            prop="title"/>
+          <el-table-column
+            label="key值"
+            prop="key">
+            <template slot-scope="scope">
+              {{ scope.row.key | fieldFormatArrayFilter }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="类型"
+            prop="type">
+            <template slot-scope="scope">
+              {{ scope.row.type | fieldTypeFilter }}
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <i
+                :class="['el-icon-arrow-up', scope.$index == 0 ? 'disabled' : '']"
+                @click="handleTableRowUp('searchForm-fields', scope.$index)"/>
+              <i
+                :class="['el-icon-arrow-down', scope.$index == searchForm.fields.length - 1 ? 'disabled' : '']"
+                @click="handleTableRowDown('searchForm-fields', scope.$index)"/>
+              <el-button
+                type="text"
+                @click="handleSearchFormFieldEdit(scope.row, scope.$index)">
+                编辑
+              </el-button>
+              <el-button
+                type="text"
+                @click="handleSearchFormFieldDelete(scope.$index, scope.row)">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <hr>
+        <h3>
+          按钮配置
+          <el-button
+            size="small"
+            type="success"
+            icon="el-icon-plus"
+            @click="handleSearchFormButtonAdd"/>
+        </h3>
+        <el-table :data="searchForm.buttons">
+          <el-table-column
+            label="按钮文本"
+            prop="text"/>
+          <el-table-column label="按钮样式">
+            <template slot-scope="scope">
+              <el-button
+                :type="scope.row.klass"
+                size="small">
+                {{ scope.row.text }}
+              </el-button>
+            </template>
+          </el-table-column>
+          <el-table-column label="按钮类型">
+            <template slot-scope="scope">
+              {{ scope.row.type | buttonTypeFilter }}
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <i
+                :class="['el-icon-arrow-up', scope.$index == 0 ? 'disabled' : '']"
+                @click="handleTableRowUp('searchForm-buttons', scope.$index)"/>
+              <i
+                :class="['el-icon-arrow-down', scope.$index == searchForm.buttons.length - 1 ? 'disabled' : '']"
+                @click="handleTableRowDown('searchForm-buttons', scope.$index)"/>
+              <el-button
+                type="text"
+                @click="handleSearchFormButtonEdit(scope.row, scope.$index)">
+                编辑
+              </el-button>
+              <el-button
+                type="text"
+                @click="handleSearchFormButtonDelete(scope.$index, scope.row)">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
     </el-tabs>
 
     <footer class="factory-footer">
@@ -90,6 +258,115 @@
         </el-form-item>
       </el-form>
     </footer>
+
+    <el-dialog
+      :visible.sync="urlDialog.visible"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      title="URL配置"
+      class="dialog-url"
+      @close="handleUrlDialogClose">
+      <el-form label-width="130px">
+        <el-form-item
+          label="接口路径"
+          prop="url">
+          <el-input
+            v-model="urlEntity.url"
+            placeholder="接口请求路径"/>
+        </el-form-item>
+        <el-form-item label="接口请求方式">
+          <el-select
+            v-model="urlEntity.method"
+            placeholder="选择请求方式">
+            <el-option
+              v-for="method in requestMethods"
+              :key="method"
+              :value="method"
+              :label="method"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          v-if="urlDialog.supportParams"
+          label="参数值">
+          <p
+            v-html="
+              urlDialog.supportDynamicParams ? `
+                简便参数提交支持三种类型，<code>{&quot;type&quot;: 1}</code>或者<code>[{&quot;name&quot;: &quot;poster_id&quot;, &quot;raw&quot;: &quot;1&quot;}]</code>固定参数，<code>[&quot;id&quot;, &quot;type&quot;]</code>附加当前环境的动态参数，<code>[{&quot;name&quot;: &quot;poster_id&quot;, &quot;value&quot;: &quot;id&quot;}]</code>需要起别名的动态参数。<br>
+                <code>name</code>是你需要替换后的名称，<code>value</code>是你当前执行环境的值的名称
+              ` : `
+                该url不支持动态参数，只能输入 <code>{ &quot;type&quot;: 1 }</code> 这样可以被转换的固定参数值
+            `
+            "/>
+          <p
+            v-if="urlDialog.paramsMessage.length > 0"
+            v-html="urlDialog.paramsMessage"/>
+          <el-input
+            v-model="urlEntity.params"
+            type="textarea"/>
+        </el-form-item>
+        <el-form-item
+          v-if="urlDialog.supportBeforeFilter"
+          label="请求前参数过滤器">
+          <p v-html="urlDialog.beforeFilterMessage"/>
+          <code-editor v-model="urlEntity.beforeFilter"/>
+        </el-form-item>
+        <el-form-item
+          v-if="urlDialog.supportFilter"
+          label="请求返回值过滤器">
+          <p v-html="urlDialog.filterMessage"/>
+          <code-editor v-model="urlEntity.filter"/>
+        </el-form-item>
+        <el-form-item
+          v-if="urlDialog.supportUrlFilter"
+          label="动态url过滤器">
+          <p>
+            某些情况下，需要根据<code>searchForm</code>的值来动态决定请求的url路径<br>
+            如果设置了动态url过滤器，则接口路径则不会生效，统一使用过滤器返回，请确保过滤器有url路径返回。
+          </p>
+          <code-editor v-model="urlEntity.urlFilter"/>
+        </el-form-item>
+        <el-form-item
+          v-if="urlDialog.supportMessages"
+          label="消息设置">
+          <p>成功时提示文案（不需要请留空）：</p>
+          <el-input
+            v-model="urlEntity.successMessage"
+            type="text"/>
+          <p>失败时提示文案（留空会显示默认报错信息）：</p>
+          <el-input
+            v-model="urlEntity.errorMessage"
+            type="text"/>
+        </el-form-item>
+        <el-form-item label="是否要二次弹框">
+          <el-radio-group v-model="urlEntity.isSecondSure">
+            <el-radio
+              v-model="urlEntity.isSecondSure"
+              label="1">是</el-radio>
+            <el-radio
+              v-model="urlEntity.isSecondSure"
+              label="2">否</el-radio>
+          </el-radio-group>
+          <div v-if="urlEntity.isSecondSure == 1">
+            <p>二次确认文案：</p>
+            <el-input
+              v-model="urlEntity.secondSureText"
+              type="text"/>
+          </div>
+        </el-form-item>
+      </el-form>
+      <div slot="footer">
+        <el-button
+          type="defualt"
+          @click="handleUrlDialogClose">
+          关闭
+        </el-button>
+        <el-button
+          type="primary"
+          @click="handleUrlDialogSave">
+          保存
+        </el-button>
+      </div>
+    </el-dialog>
 
     <el-dialog
       :visible.sync="navDialog.visible"
--- src/page/js/factoryScreen.js
+++ src/page/js/factoryScreen.js
@@ -8,6 +8,7 @@ import Helper from '../../lib/helper';
 import CodeEditor from '../../components/CodeEditor'
 import router from '../../router'
 
+// 页面导航配置
 const defaultNavDialog = {
   visible: false,
   save() {}
@@ -23,11 +24,55 @@ const defaultNavEntity = {
   active: false
 }
 
+// 默认配置请求实体的弹出框配置
+const defaultUrlDialog = {
+  visible: false,
+  supportParams: true,
+  supportDynamicParams: true,
+  paramsMessage: '',
+  supportMessages: true,
+  supportBeforeFilter: true,
+  beforeFilterMessage: '',
+  supportFilter: true,
+  filterMessage: '',
+  supportUrlFilter: false,
+  save() {}
+}
+
+// 默认请求实体
+const defaultUrlEntity = {
+  url: '', // 请求路径
+  method: 'GET', // 请求的method
+  params: '', // 请求参数（用于简易情况）
+  urlFilter: '', // 动态url过滤器
+  beforeFilter: '', // 请求前的过滤器，用于返回请求的参数
+  filter: '', // 请求后的过滤器，用于处理返回值的填充
+  successMessage: '', // 请求成功的提示文案
+  errorMessage: '', // 请求错误的提示文案
+  isSecondSure: '2', // 是否需要二次确认弹框
+  secondSureText: '是否确定保存' // 二次确认文案
+}
+
 export default {
   name: 'FactoryScreen',
+
   components: {
     CodeEditor
   },
+
+  filters: {
+    searchFormUrlTypeFilter(val) {
+      const map = {
+        list: '列表',
+        chart: '图表',
+        total: '计数',
+        summary: '汇总'
+      }
+
+      return map[val]
+    }
+  },
+
   data() {
     return {
       loading: false,
@@ -40,18 +85,67 @@ export default {
 
       disabledPageCode: false,
 
+      requestMethods: ['GET', 'POST', 'DELETE', 'HEAD', 'PUT', 'PATCH'],
+
       // 页面配置项目按的验证配置
       pageRules: {
         title: [{ required: true, message: '请输入页面标题', trigger: 'blur' }],
         url: [{ required: true, message: '请输入页面url', trigger: 'blur' }]
       },
 
+      // 搜索表单配置选项的验证配置
+      searchFormRules: {
+        title: [
+          { required: true, message: '请输入页面标题', trigger: 'blur' }
+        ]
+      },
+
       // 页面配置选项
       page: Helper.assign({}, Config.setup.page),
 
       navDialog: Helper.assign({}, defaultNavDialog),
-      navEntity: Helper.assign({}, defaultNavEntity)
+      navEntity: Helper.assign({}, defaultNavEntity),
+
+      // 搜索表单配置选项
+      searchForm: Helper.assign({}, Config.setup.searchForm),
+
+      urlDialog: Helper.assign({}, defaultUrlDialog),
+      urlEntity: Helper.assign({}, defaultUrlEntity)
+
     };
+  },
+
+  computed: {
+    searchFormUrls() {
+      const result = []
+
+      Object.keys(this.searchForm.urls).forEach((u) => {
+        const url = this.searchForm.urls[u]
+        let r = {
+          type: u,
+          raw: url
+        }
+        if (Object.keys(url).length) {
+          r = Helper.assign(r, {
+            config: true,
+            url: url.url,
+            method: url.method,
+            params: JSON.stringify(url.params)
+          })
+        } else {
+          r = Helper.assign(r, {
+            config: false,
+            url: '',
+            method: '',
+            params: ''
+          })
+        }
+
+        result.push(r)
+      })
+
+      return result
+    }
   },
   methods: {
     renderNavOperationColumn(h) {
@@ -70,7 +164,7 @@ export default {
     },
 
     /**
-     * @description: page dialog function navDialog.visible
+     * @description: page配置 && page顶部导航弹窗方法 navDialog.visible
      * @param {type}
      * @return:
      * @author: PoloHuang
@@ -111,6 +205,107 @@ export default {
     },
 
     /**
+     * @description: searchForm配置 && 请求接口配置操作弹窗方法 navDialog.visible
+     * @param {type}
+     * @return:
+     * @author: PoloHuang
+     */
+
+    handleSearchFormUrlConfig(row, type = 'searchForm') {
+      console.log(row)
+      this.urlDialog.beforeFilterMessage = `
+        函数会传入 <code>params</code> 作为参数。该参数是搜索表单的值，必须要返回一个对象，作为查询条件。<br>
+        如果不需要做过滤，请留空。
+      `
+
+      if (row.type === 'list' || row.type === 'chart') {
+        this.urlDialog.filterMessage = `
+          函数会传入 <code>res</code>, <code>params</code>, <code>url</code>作为参数。该参数是服务端请求返回值，必须要返回一个对象，作为数据列表的数据集。<br>
+          示例：<code>{ data: [], total: 0 }</code> 返回值对象中total不是必须属性，如果配置了计数的请求接口的话。<br>
+          如果不需要做过滤，请留空。
+        `
+      }
+
+      if (row.type == 'summary') {
+        this.urlDialog.filterMessage = `
+          函数会传入 <code>res</code>, <code>params</code>, <code>url</code>作为参数。该参数是服务端请求返回值，必须要返回一个对象，作为数据总计的数据集。<br>
+          如果不需要做过滤，请留空。
+        `
+      }
+
+      this.urlDialog.supportUrlFilter = true
+
+      const urlEntity = Helper.assign({}, defaultUrlEntity, row.raw)
+      console.log(urlEntity)
+
+      if (typeof urlEntity.params == 'object') {
+        urlEntity.params = JSON.stringify(urlEntity.params)
+      }
+      this.urlEntity = urlEntity
+
+      this.urlDialog.save = (urlInfo) => {
+        if (type == 'searchForm') {
+          this.searchForm.urls[row.type] = urlInfo
+        } else if (type == 'dataTableSort') {
+
+        }
+      }
+
+      this.urlDialog.visible = true
+    },
+
+    // 处理接口请求配置框的关闭动作
+    handleUrlDialogClose() {
+      this.urlEntity = Helper.assign({}, defaultUrlEntity)
+      this.urlDialog = Helper.assign({}, defaultUrlDialog)
+    },
+
+    // 处理接口请求配置框的保存动作
+    handleUrlDialogSave() {
+      const urlInfo = Helper.assign({}, this.urlEntity)
+      if (urlInfo.params.trim()) {
+        try {
+          urlInfo.params = JSON.parse(urlInfo.params)
+        } catch (e) {
+          this.$alert('url的params无法被转换，请检查', '警告')
+          console.error(e)
+          return
+        }
+      } else {
+        urlInfo.params = {}
+      }
+
+      urlInfo.beforeFilter = urlInfo.beforeFilter.trim()
+      urlInfo.filter = urlInfo.filter.trim()
+      this.urlDialog.save(urlInfo)
+      this.$message({
+        type: 'success',
+        message: '保存请求接口配置成功'
+      })
+      this.handleUrlDialogClose()
+    },
+
+    handleClearUrlConfig(origin, urlInfo) {
+      if (origin === 'searchForm') {
+        this.$confirm('是否清空该url配置', '提示', {
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.searchForm.urls[urlInfo.type] = {}
+        }).catch(() => {})
+      }
+    },
+
+    handleSearchFormFieldAdd() {
+
+    },
+
+    handleSearchFormButtonAdd() {
+
+    },
+
+    /**
      * @description: handleSave
      * @param {type}
      * @return:
@@ -118,7 +313,8 @@ export default {
      */
     handleSave() {
       const pageConfig = JSON.stringify({
-        page: this.page
+        page: this.page,
+        searchForm: this.searchForm
       });
       const params = {
         type: Config.metaTypes.page,
@@ -213,6 +409,7 @@ export default {
       loadLocalConfig()
 
     }
+
   },
 
   mounted() {
